e676abfb1680067a6f3de4454c903ca3c3b532d9
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-01-05 09:47:29 +0300

    integrate react router library

--- Countries/src/App.jsx
+++ Countries/src/App.jsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Homepage from "./pages/Homepage";
+import CountryPage from "./pages/CountryPage";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/:country-id" element={<CountryPage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
