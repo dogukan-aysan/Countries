bb7942cfebec513daf02bcf568e5e2f6254a0d03
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-01-07 09:58:53 +0300

    set up the react-query devtools

--- Countries/src/App.jsx
+++ Countries/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import Homepage from "./pages/Homepage";
 import CountryPage from "./pages/CountryPage";
@@ -17,6 +18,7 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
--- Countries/src/components/CountryGrid.jsx
+++ Countries/src/components/CountryGrid.jsx
@@ -1,6 +1,9 @@
+import useCountries from "../hooks/useCountries";
 import CountryCard from "./CountryCard";
 
 function CountryGrid() {
+  const { data, isLoading, isError } = useCountries();
+  console.log(data);
   return (
     <div className="main__grid">
       <CountryCard />
