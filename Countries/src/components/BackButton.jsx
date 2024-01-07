250c2f5b454b7b7e3d881c93d9ebbabbe9940a20
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-01-07 07:15:42 +0300

    style back button

--- Countries/src/components/BackButton.jsx
+++ Countries/src/components/BackButton.jsx
@@ -1,5 +1,12 @@
+import { MdOutlineKeyboardBackspace } from "react-icons/md";
+
 function BackButton() {
-  return <div className="country-page__back-button">BackButton</div>;
+  return (
+    <div className="country-page__back-button">
+      <MdOutlineKeyboardBackspace />
+      <span>Back</span>
+    </div>
+  );
 }
 
 export default BackButton;
