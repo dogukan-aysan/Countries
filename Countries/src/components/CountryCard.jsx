7ce63ccf651b0c31067728ca6d5d17623a8dde28
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-01-07 08:23:07 +0300

    hack: divide country-info and country-card overviews

--- Countries/src/components/CountryCard.jsx
+++ Countries/src/components/CountryCard.jsx
@@ -8,15 +8,15 @@ function CountryCard() {
         alt="flag"
       />
       <div className="country-card__overview">
-        <h3 className="overview__heading">Türkiye</h3>
-        <ul className="overview__list">
-          <li className="overview__list-item">
+        <h3 className="country-card__heading">Türkiye</h3>
+        <ul className="country-card__list">
+          <li className="country-card__list-item">
             <span className="list-item__field">Population: </span>84339067
           </li>
-          <li className="overview__list-item">
+          <li className="country-card__list-item">
             <span className="list-item__field">Region: </span>Asia
           </li>
-          <li className="overview__list-item">
+          <li className="country-card__list-item">
             <span className="list-item__field">Capital: </span>Ankara
           </li>
         </ul>
