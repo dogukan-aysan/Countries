79ef54c9c56628dcd23f99f492c199a951c3ae9c
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-01-07 10:03:51 +0300

    get queried data into the CountryGrid component

--- Countries/src/components/CountryGrid.jsx
+++ Countries/src/components/CountryGrid.jsx
@@ -4,16 +4,13 @@ import CountryCard from "./CountryCard";
 function CountryGrid() {
   const { data, isLoading, isError } = useCountries();
   console.log(data);
+  if (isLoading) return <div>LOADING...</div>;
+  if (isError) return <div>ERROR</div>;
   return (
     <div className="main__grid">
-      <CountryCard />
-      <CountryCard />
-      <CountryCard />
-      <CountryCard />
-      <CountryCard />
-      <CountryCard />
-      <CountryCard />
-      <CountryCard />
+      {data.map((country, i) => (
+        <CountryCard key={i} />
+      ))}
     </div>
   );
 }
