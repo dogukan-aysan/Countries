35a9bb895266fb9841c79606d113d5e9776650c4
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-01-07 09:05:41 +0300

    set up react-query

--- Countries/src/App.jsx
+++ Countries/src/App.jsx
@@ -1,18 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import Homepage from "./pages/Homepage";
 import CountryPage from "./pages/CountryPage";
 import PageNotFound from "./pages/PageNotFound";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/country-details/:id" element={<CountryPage />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/country-details/:id" element={<CountryPage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
