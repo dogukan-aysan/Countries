8dd74d62f300c76949073952c8be02a58d863c68
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-01-08 08:49:19 +0300

    reset country data after leaving 'country-details' page

--- Countries/src/pages/Homepage.jsx
+++ Countries/src/pages/Homepage.jsx
@@ -1,9 +1,15 @@
+import { useQueryClient } from "@tanstack/react-query";
 import CountryGrid from "../components/CountryGrid";
 import Filter from "../components/Filter";
 import Header from "../components/Header";
 import SearchBox from "../components/SearchBox";
+import { useEffect } from "react";
 
 function Homepage() {
+  const queryClient = useQueryClient();
+  useEffect(() => {
+    queryClient.resetQueries("country");
+  }, [queryClient]);
   return (
     <div className="homepage">
       <Header />
