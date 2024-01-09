53e12a82d348cfed0377f3a231f1de92a971bbb3
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-01-09 13:44:36 +0300

    re-write Filter component in order to implement selection and dispatching

--- Countries/src/components/Filter.jsx
+++ Countries/src/components/Filter.jsx
@@ -1,14 +1,35 @@
+import { useContext, useEffect, useState } from "react";
+import { CountryContext } from "../context/CountryContext";
+
 function Filter() {
+  const [selected, setSelected] = useState("");
+  const { dispatch } = useContext(CountryContext);
+  useEffect(() => {
+    dispatch({ type: "region", payload: selected });
+  }, [selected, dispatch]);
   return (
-    <select className="grid-header__filter">
+    <select
+      className="grid-header__filter"
+      onChange={(e) => setSelected(e.target.value)}
+    >
       <option className="grid-header__filter-item" defaultValue={true} hidden>
         Filter by Region
       </option>
-      <option className="grid-header__filter-item">Africa</option>
-      <option className="grid-header__filter-item">America</option>
-      <option className="grid-header__filter-item">Asia</option>
-      <option className="grid-header__filter-item">Europe</option>
-      <option className="grid-header__filter-item">Oceania</option>
+      <option className="grid-header__filter-item" value="africa">
+        Africa
+      </option>
+      <option className="grid-header__filter-item" value="america">
+        America
+      </option>
+      <option className="grid-header__filter-item" value="asia">
+        Asia
+      </option>
+      <option className="grid-header__filter-item" value="europe">
+        Europe
+      </option>
+      <option className="grid-header__filter-item" value="oceania">
+        Oceania
+      </option>
     </select>
   );
 }
