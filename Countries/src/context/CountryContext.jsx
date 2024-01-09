ac5ffd3ef9682ee6cc9cc6f617f96eb88310dcf8
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-01-09 10:37:35 +0300

    add new variable to the context

--- Countries/src/context/CountryContext.jsx
+++ Countries/src/context/CountryContext.jsx
@@ -2,12 +2,15 @@ import { createContext, useReducer } from "react";
 
 const initialState = {
   selectedCountry: "",
+  searchedText: "",
 };
 
 function reducer(state, action) {
   switch (action.type) {
     case "country/selected":
       return { ...state, selectedCountry: action.payload };
+    case "searched":
+      return { ...state, searchedText: action.payload };
     default:
       return { ...state };
   }
@@ -16,9 +19,14 @@ function reducer(state, action) {
 const CountryContext = createContext();
 
 const CountryProvider = ({ children }) => {
-  const [{ selectedCountry }, dispatch] = useReducer(reducer, initialState);
+  const [{ selectedCountry, searchedText }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
   return (
-    <CountryContext.Provider value={{ selectedCountry, dispatch }}>
+    <CountryContext.Provider
+      value={{ selectedCountry, searchedText, dispatch }}
+    >
       {children}
     </CountryContext.Provider>
   );
