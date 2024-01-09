3278e09d1bdc3974132258d64ef5468e3efdcf0a
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-01-09 08:42:49 +0300

    make SearchBox component stateful

--- Countries/src/components/SearchBox.jsx
+++ Countries/src/components/SearchBox.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 
 function SearchBox() {
+  const [typedText, setTypedText] = useState();
+  const handleChange = (text) => {
+    setTypedText(text);
+  };
   return (
     <div className="grid-header__search-box">
       <span className="search-box__magnifying-glass">
@@ -10,6 +15,8 @@ function SearchBox() {
         className="search-box__input"
         type="text"
         placeholder="Search for a country..."
+        value={typedText}
+        onChange={(e) => handleChange(e.target.value)}
       />
     </div>
   );
