8360f22cfc22fc58f44feee9f8a24509617b41b3
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-01-09 12:17:13 +0300

    reset searched text upon leaving homepage

--- Countries/src/pages/CountryPage.jsx
+++ Countries/src/pages/CountryPage.jsx
@@ -1,8 +1,17 @@
+import { useQueryClient } from "@tanstack/react-query";
 import BackButton from "../components/BackButton";
 import CountryInfo from "../components/CountryInfo";
 import Header from "../components/Header";
+import { useContext, useEffect } from "react";
+import { CountryContext } from "../context/CountryContext";
 
 function CountryPage() {
+  const queryClient = useQueryClient();
+  const { dispatch } = useContext(CountryContext);
+  useEffect(() => {
+    queryClient.resetQueries("searchedCountries");
+    dispatch({ type: "searched", payload: "" });
+  }, [queryClient, dispatch]);
   return (
     <div className="country-page">
       <Header />
