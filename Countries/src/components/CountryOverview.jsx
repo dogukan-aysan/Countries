ddc20fe5233d8dad04d47dcb7f0ea43a3a6aaffc
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-01-10 09:37:03 +0300

    refactor: change class names

--- Countries/src/components/CountryOverview.jsx
+++ Countries/src/components/CountryOverview.jsx
@@ -14,37 +14,43 @@ function CountryOverview({ country }) {
       <h3 className="country-info__heading">{name.official}</h3>
       <ul className="country-info__list">
         <li className="country-info__list-item">
-          <span className="list-item__field">Native Name: </span>
-          {Object.values(name.nativeName)[0].official}
+          <span className="country-info__field">Native Name: </span>
+          <span className="country-info__value">
+            {Object.values(name.nativeName)[0].official}
+          </span>
         </li>
         <li className="country-info__list-item">
-          <span className="list-item__field">Population: </span>
-          {population}
+          <span className="country-info__field">Population: </span>
+          <span className="country-info__value">{population}</span>
         </li>
         <li className="country-info__list-item">
-          <span className="list-item__field">Region: </span>
-          {region}
+          <span className="country-info__field">Region: </span>
+          <span className="country-info__value">{region}</span>
         </li>
 
         <li className="country-info__list-item">
-          <span className="list-item__field">Sub Region: </span>
-          {subregion}
+          <span className="country-info__field">Sub Region: </span>
+          <span className="country-info__value">{subregion}</span>
         </li>
         <li className="country-info__list-item">
-          <span className="list-item__field">Capital: </span>
-          {capital}
+          <span className="country-info__field">Capital: </span>
+          <span className="country-info__value">{capital}</span>
         </li>
         <li className="country-info__list-item">
-          <span className="list-item__field">Top Level Domain: </span>
-          {tld}
+          <span className="country-info__field">Top Level Domain: </span>
+          <span className="country-info__value">{tld}</span>
         </li>
         <li className="country-info__list-item">
-          <span className="list-item__field">Currencies: </span>
-          {Object.values(currencies)[0].name}
+          <span className="country-info__field">Currencies: </span>
+          <span className="country-info__value">
+            {Object.values(currencies)[0].name}
+          </span>
         </li>
         <li className="country-info__list-item">
-          <span className="list-item__field">Languages: </span>
-          {Object.values(languages)[0]}
+          <span className="country-info__field">Languages: </span>
+          <span className="country-info__value">
+            {Object.values(languages)[0]}
+          </span>
         </li>
       </ul>
     </div>
